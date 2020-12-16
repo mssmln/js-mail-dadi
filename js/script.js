c37466ff1fadafe8f7d09b365ebf400fb35b8a3e
Massimiliano <m.croci.ly@gmail.com>
2020-12-16 17:53:24 +0100

    addEventListener

--- js/script.js
+++ js/script.js
@@ -3,19 +3,29 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Completate inoltre l'esercizio di ieri e rivedete quanto svolto a lezione.
 // Buon divertimento :ferris_wheel:
-
-
-// dichiara le variabili per l'essere umano e il computer, assegna un numero random compreso tra 1 e 6
-var playerHuman = Math.floor(Math.random() * 6) + 1;
-var playerComputer = Math.floor(Math.random() * 6) + 1;
+// i dadi siano lanciati al click di un pulsante
 
 
 
-//con una condizione if verifica il vincitore, il perdente e se si verifica una parità ed infine stampa in html il risultato del lancio dei dadi
-if (playerHuman > playerComputer) {
-  document.getElementById('human').innerHTML = 'il vincitore del lancio dei dadi è playerHuman';
-} else if (playerComputer > playerHuman) {
-  document.getElementById('human').innerHTML = 'il vincitore del lancio dei dadi è playerComputer';
-} else {
-  document.getElementById('equality').innerHTML = 'nessun vincitore, risultato di parità, rilanciare i dadi';
-}
+// throw the dice when clicking on the button
+var throwTheDice = document.getElementById('dice');
+throwTheDice.addEventListener('click',
+  function () {
+
+
+    // dichiara le variabili per l'essere umano e il computer, assegna un numero random compreso tra 1 e 6
+    var playerHuman = Math.floor(Math.random() * 6) + 1;
+    var playerComputer = Math.floor(Math.random() * 6) + 1;
+
+
+
+    //con una condizione if verifica il vincitore, il perdente e se si verifica una parità ed infine stampa in html il risultato del lancio dei dadi
+    if (playerHuman > playerComputer) {
+      document.getElementById('human').innerHTML = 'il vincitore del lancio dei dadi è playerHuman';
+    } else if (playerComputer > playerHuman) {
+      document.getElementById('human').innerHTML = 'il vincitore del lancio dei dadi è playerComputer';
+    } else {
+      document.getElementById('equality').innerHTML = 'nessun vincitore, risultato di parità, rilanciare i dadi';
+    }
+  }
+);
